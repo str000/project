e6056b2f677975863b1ea204c56731b1c3a9fdc6
RadzioN <radeknapora2003@gmail.com>
2021-09-28 22:14:00 +0200

    material-module
    Plik material-module.ts dodaje wszystkie komponenty z Material Module. Najpierw trzeba go zainstalować.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { DemoMaterialModule } from './material-module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,9 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
