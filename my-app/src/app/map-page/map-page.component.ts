a2b9f3a6bbc1371ea6825d55ab120470466c7c6c
RadzioN <radeknapora2003@gmail.com>
2021-10-02 11:08:48 +0200

    Marker i Lokalizacja

--- my-app/src/app/map-page/map-page.component.ts
+++ my-app/src/app/map-page/map-page.component.ts
@@ -10,15 +10,43 @@ import { environment } from 'src/environments/environment';
 export class MapPageComponent implements OnInit {
 
   mapa: Mapboxgl.Map;
-
+  lat: any;
+  lng: any;
+marker1: any;
   ngOnInit() {
-    this.mapa = new Mapboxgl.Map({
-      accessToken: environment.mapboxKey,
-      container: 'mapa-mapbox',
-      style: 'mapbox://styles/radek03/cku8qabln2n3b17o4v0ahpmc2',
-      center: [19.017979, 50.258462],
-      zoom: 16.52
-    })
+
+    if("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.mapa = new Mapboxgl.Map({
+          accessToken: environment.mapboxKey,
+          container: 'mapa-mapbox',
+          style: 'mapbox://styles/radek03/cku8qabln2n3b17o4v0ahpmc2',
+          center: [position.coords.longitude, position.coords.latitude],
+          zoom: 15
+        });
+
+        const el = document.createElement('div');
+        el.className = 'marker';
+
+        this.marker1 = new Mapboxgl.Marker(el)
+          .setLngLat([position.coords.longitude, position.coords.latitude])
+          .addTo(this.mapa);
+      });
+      navigator.geolocation.getCurrentPosition(position => {
+        this.marker1.setLngLat([position.coords.longitude, position.coords.latitude]);
+      })
+    } else {
+      //nie zezwolono na lokalizowanie
+    }
+
+ 
+
+    
+
+    
+
+      
   }
+
 
 }
