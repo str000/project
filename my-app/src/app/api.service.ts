57181f497c860f7b9059333b852461bb7e1197b5
str000 <str000@protonmail.com>
2021-10-04 19:15:46 +0200

    1

--- my-app/src/app/api.service.ts
+++ my-app/src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Data } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   constructor(private httpClient: HttpClient) { }
 
-  public getCity(){return this.httpClient.get('https://api.waqi.info/feed/here/?token=568e92e9746cdbf8c8ba03e55b2569d0a2c3a254');}
+  public getCity(): Observable<Data[]>{
+    return this.httpClient.get<Data[]>('https://api.waqi.info/feed/here/?token=568e92e9746cdbf8c8ba03e55b2569d0a2c3a254');
+  }
 }
--- my-app/src/app/profil-page/profil-page.component.ts
+++ my-app/src/app/profil-page/profil-page.component.ts
@@ -10,11 +10,21 @@ export class ProfilPageComponent implements OnInit {
 
   cityValue: any = [ ];
 
+  popValue: any = [ ];
+
+  setColor: boolean = true;
+
   constructor(private apiService: ApiService) { }
   ngOnInit() {
     this.apiService.getCity().subscribe((data)=>{
       this.cityValue.push(data);
       console.log(this.cityValue);
+
+      //let popValue = this.cityValue.map((t: { iaqi: { o3: { v: any; }; }; }) => t.iaqi.o3.v)
+      //console.log(popValue);
+
+      //this.popValue = JSON.parse(this.cityValue)
+      //console.log(this.popValue)
     });
   }
 
